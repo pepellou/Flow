62e7980129687b2cb1dba6b6410b5d384085bd1a
Pepe Doval <pepellou@gmail.com>
2011-09-20 03:58:17 +0200

    Copied 'normal' style to mobile

--- mobile.php
+++ mobile.php
@@ -1,5 +1,39 @@
-<?php
-
-	echo "Mobile version still not available";
-
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html 
+   xmlns    = "http://www.w3.org/1999/xhtml" 
+   lang     = "_LANGUAGE" 
+   xml:lang = "_LANGUAGE">
+	<head>
+		<title>Flow</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+		<link rel="stylesheet" 
+			href="css/style-mobile.css" 
+			type="text/css" />
+		<link rel="author" href="humans.txt" />
+		<link rel="icon" type="image/gif" href="favicon.gif" />
+	</head>
+	<body>
+		<div id=title>
+			<h1><a href=?page=home>Flow</a></h1>
+		</div>
+		<div id=content>
+			<div id=humanstxt>
+				<a href=humans.txt>
+					<img width="88" 
+						height="31" 
+						alt="Humans.txt" 
+						src="http://humanstxt.org/img/oficial-logos/humanstxt-isolated-blank.gif">
+				</a>
+			</div>
+			<?php 
+				$page = (isset($_GET['page'])
+					? $_GET['page']
+					: 'home'
+				);
+				include("pages/$page.php");
+			?>
+		</div>
+	</body>
+</html>
