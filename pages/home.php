1eb8f4a0565ba0b12e981776844978b201d5216d
Pepe Doval <pepellou@gmail.com>
2011-09-20 03:07:53 +0200

    Home - selection page

--- pages/home.php
+++ pages/home.php
@@ -1,6 +1,13 @@
-<div id=what>
-	<h2>What is this?</h2>
-	<p>
-		Marnie would tell...
-	</p>
-</div>
+<h2>Who are you?</h2>
+<center>
+	<table>
+		<tr>
+			<td><a href=?page=question&who=producer><img src=img/producer.png /></a></td>
+			<td><a href=?page=question&who=junkie><img src=img/junkie.png /></a></td>
+		</tr>
+		<tr>
+			<td><a href=?page=question&who=dealer><img src=img/dealer.png /></a></td>
+			<td><a href=?page=question&who=whore><img src=img/whore.png /></a></td>
+		</tr>
+	</table>
+</center>
