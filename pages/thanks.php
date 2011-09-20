7a6bbf34552de4d81b85837fc740b9d2451458ea
Pepe Doval <pepellou@gmail.com>
2011-09-20 03:20:00 +0200

    Added thanks page

---
+++ pages/thanks.php
@@ -0,0 +1,10 @@
+<h2>Thanks</h2>
+<center>
+	<table border=0>
+		<tr>
+			<td>Your wishes will be <br/>considered... maybe</td>
+			<td width=50px></td>
+			<td><img src=img/genius.png /></td>
+		</tr>
+	</table>
+</center>
