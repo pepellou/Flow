c1790cbcbe162a9966a2ee71004a251c4f355611
unknown <Oimel@Guarra.(none)>
2011-10-26 14:13:19 +0200

    Hide error reporting

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php include_once('mobile-detection.php'); ?>
+<?php 
+	include_once('mobile-detection.php');
+	error_reporting(0);
+ ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html 
